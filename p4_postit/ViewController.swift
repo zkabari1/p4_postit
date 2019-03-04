3b18c0a9f50f0e6cd257a16dd49a21466e72cebd
Zimpleprakashb Kabariya <zkabari1@bl-n22.pods.bu.int>
2019-03-04 09:12:25 -0500

    Initial Commit

---
+++ p4_postit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  p4_postit
+//
+//  Created by Zimpleprakashb Kabariya on 3/4/19.
+//  Copyright © 2019 SUNY Binghamton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
